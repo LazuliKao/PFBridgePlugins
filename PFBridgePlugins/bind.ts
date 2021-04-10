6f25d167e38bfc6acd15183cbf3385a7a6f7b97a
littlegao233 <gaoxinhong2004@hotmail.com>
2021-04-11 00:05:25 +0800

    sync

--- PFBridgePlugins/bind.ts
+++ PFBridgePlugins/bind.ts
@@ -1,3 +1,5 @@
-/// <reference path="./PFBridgeCore/index.ts" />
+/// <reference path="./PFBridgeCore/index.d.ts" />
 const a=System.IO
-a.File.WriteAllText("aa.json","",System.Text.Encoding.UTF8)
+a.File.WriteAllText("aa.json", "", System.Text.Encoding.UTF8)
+const x = a.File.ReadAllText
+
